3e4958df7ac92937caff3ef4c8d19290ce1c9188
emiliano-bn <oscar.bernab3@gmail.com>
2025-10-03 15:41:41 -0600

    campos para facturacion

---
+++ efacturamx/efacturamx/doctype/forma_de_pago/test_forma_de_pago.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Naawa Consulting and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFormadePago(FrappeTestCase):
+	pass
