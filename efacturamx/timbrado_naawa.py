01682a482892e145161fae3ea5ddc78cde065169
emiliano-bn <oscar.bernab3@gmail.com>
2025-10-30 18:08:27 -0600

    fixed links and print format

--- efacturamx/timbrado_naawa.py
+++ efacturamx/timbrado_naawa.py
@@ -7,15 +7,20 @@ from frappe.utils import cint
 def timbrar(docname, doctype):
     doc = frappe.get_doc(doctype, docname)
 
-    # Serializar usando el encoder de frappe (maneja datetime y otros tipos)
+    company_name = "INDISTRIA ILUMINADORA DE ALMACENES"
+    servidor_timbrado = frappe.db.get_value("Company", company_name, "servidor_timbrado")
+
+    if not servidor_timbrado:
+        frappe.throw(f"No se encontró el campo 'servidor_timbrado' en la empresa {company_name}")
+
     datos = json.loads(frappe.as_json(doc.as_dict()))
 
-    webhook_url = "https://n8n.naawaconsulting.dev/webhook-test/erpnext"
+    webhook_url = servidor_timbrado
     headers = {"Content-Type": "application/json"}
 
     try:
         response = requests.post(webhook_url, json=datos, headers=headers)
         response.raise_for_status()
-        return "webhook activado"
+        return "webhook activado correctamente"
     except Exception as e:
         frappe.throw(f"Error al activar webhook: {str(e)}")
