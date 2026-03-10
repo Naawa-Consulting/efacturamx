89ac69c5e0e37e55dd32bce84de29d1490570040
emiliano-bn <oscar.bernab3@gmail.com>
2026-03-10 16:16:04 -0600

    fix

--- efacturamx/public/js/facturacion_sales_invoice.js
+++ efacturamx/public/js/facturacion_sales_invoice.js
@@ -1,5 +1,7 @@
 frappe.ui.form.on("Sales Invoice", {
+
     timbrar: function(frm) {
+
         frappe.call({
             method: "efacturamx.timbrado_naawa.timbrar",
             args: {
@@ -7,35 +9,75 @@ frappe.ui.form.on("Sales Invoice", {
                 doctype: frm.doc.doctype,
                 site_url: window.location.origin
             },
+            freeze: true,
+            freeze_message: "Timbrando...",
             callback: function(r) {
-                frappe.msgprint(r.message || "Timbrando...");
-                frm.reload_doc();
+
+                frm.reload_doc().then(() => {
+
+                    if (frm.doc.custom_uuid) {
+
+                        frappe.msgprint({
+                            title: __('Éxito'),
+                            message: 'Factura Timbrada',
+                            indicator: 'green'
+                        });
+
+                    } else if (frm.doc.error) {
+
+                        frappe.msgprint({
+                            title: __('Error en Timbrado'),
+                            message: frm.doc.error,
+                            indicator: 'red'
+                        });
+
+                    }
+
+                });
+
             }
         });
+
     },
 
     cancelar_timbre: function(frm) {
+
         frappe.call({
-            method: "efacturamx.timbrado_naawa.timbrar",
+            method: "efacturamx.timbrado_naawa.cancelar_timbre",
             args: {
                 docname: frm.doc.name,
                 doctype: frm.doc.doctype,
-                site_url:window.location.origin
+                site_url: window.location.origin
             },
+            freeze: true,
+            freeze_message: "Cancelando Timbre...",
             callback: function(r) {
-                frappe.msgprint(r.message || "Cancelando timbre...");
-                frm.reload_doc();
+
+                frm.reload_doc().then(() => {
+
+                    if (frm.doc.uuid_cancelacion) {
+
+                        frappe.msgprint({
+                            title: __('Éxito'),
+                            message: 'Factura Cancelada',
+                            indicator: 'green'
+                        });
+
+                    } else if (frm.doc.error) {
+
+                        frappe.msgprint({
+                            title: __('Error en Cancelación'),
+                            message: frm.doc.error,
+                            indicator: 'red'
+                        });
+
+                    }
+
+                });
+
             }
         });
-    },
 
-    error(frm) {
-    if (frm.doc.error) {
-        frappe.msgprint({
-            title: __('Error en Timbrado'),
-            message: frm.doc.error,
-            indicator: 'red'
-        });
     }
-    }
+
 });
